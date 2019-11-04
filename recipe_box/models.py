cd39a64146da096c78ce37b10c902fa1b1786121
Matthew Perry <perry357@outlook.com>
2019-11-04 14:54:00 -0500

    started authentication process

--- recipe_box/models.py
+++ recipe_box/models.py
@@ -1,7 +1,7 @@
 """
 Chef
     Name
-
+    Bio
 
 Recipes
     Date
@@ -12,10 +12,12 @@ Recipes
 """
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Chef(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.TextField()
+    bio = models.TextField(blank=True, null=True)
     def __str__(self):
         return f"{self.name}"
 
@@ -29,5 +31,3 @@ class Recipe(models.Model):
 
     def __str__(self):
         return f"{self.recipe_name}"
-
-
